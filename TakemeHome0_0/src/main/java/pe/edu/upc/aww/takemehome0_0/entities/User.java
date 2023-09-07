ba993db8257c438f4b2b09e50ab15d3e8e3d2f91
bananin26 <mabeblue26@gmail.com>
2023-09-07 00:12:35 -0500

    user

--- TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/entities/User.java
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/entities/User.java
@@ -1,4 +1,11 @@
 package pe.edu.upc.aww.takemehome0_0.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUser;
 }
