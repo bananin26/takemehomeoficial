d42700f11dbfc06df16da8a5c9baa7bf933138b0
PieroLoa <piero.loarte32@gmail.com>
2023-09-08 09:41:47 -0500

    Hola

---
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/serviceinterfaces/IMessageService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aww.takemehome0_0.serviceinterfaces;
+
+import pe.edu.upc.aww.takemehome0_0.entities.Message;
+import pe.edu.upc.aww.takemehome0_0.serviceimplements.MessageServiceImplement;
+
+import java.util.List;
+
+public interface IMessageService {
+    public void insert(Message message);
+    public List<Message> list();
+    public Message listId(int idMessage);
+    public void delete(int idMessage);
+}
