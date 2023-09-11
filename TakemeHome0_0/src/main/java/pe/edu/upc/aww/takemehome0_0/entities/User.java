d49218273a9e19f1934c2c63a9e08840b41cf529
bananin26 <mabeblue26@gmail.com>
2023-09-10 23:00:03 -0500

    User :)

--- TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/entities/User.java
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/entities/User.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.aww.takemehome0_0.entities;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name="users")
@@ -8,4 +9,97 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idUser;
+    @Column(name="name",length = 45,nullable = false)
+    private String name;
+    @Column(name="username",nullable = false)
+    private String username;
+    @Column(name = "email",nullable = false)
+    private String email;
+    @Column(name = "phone",nullable = false)
+    private String phone;
+    @Column(name = "birthday",nullable = false)
+    private LocalDate birthday;
+    @Column(name = "country",nullable = false)
+    private String country;
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public User(int idUser, String name, String username, String email,String phone, LocalDate birthday, String country, String password) {
+
+        this.idUser= idUser;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.phone=phone;
+        this.birthday = birthday;
+        this.country = country;
+        this.password = password;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
