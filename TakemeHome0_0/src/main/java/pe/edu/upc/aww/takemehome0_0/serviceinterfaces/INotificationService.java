e95020c1a86861f28065ccd1d760fa3c5630f34d
bananin26 <mabeblue26@gmail.com>
2023-09-12 00:06:52 -0500

    records

--- TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/serviceinterfaces/INotificationService.java
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/serviceinterfaces/INotificationService.java
@@ -3,7 +3,7 @@ package pe.edu.upc.aww.takemehome0_0.serviceinterfaces;
 import pe.edu.upc.aww.takemehome0_0.entities.Notification;
 
 public interface INotificationService {
-    public void Insert(Notification notification);
+    public void insert(Notification notification);
     public void delete(int idNotification);
     public Notification listId(int idNotification);
 }
