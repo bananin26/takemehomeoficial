2c6e62c3418ef251f64f2f97585aa58da1cb64f8
bananin26 <mabeblue26@gmail.com>
2023-09-16 22:59:08 -0500

    all

--- TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/entities/User.java
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/entities/User.java
@@ -2,13 +2,14 @@ package pe.edu.upc.aww.takemehome0_0.entities;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idUser;
+    private Long id;
     @Column(name="name",length = 45,nullable = false)
     private String name;
     @Column(name = "email",nullable = false)
@@ -19,25 +20,24 @@ public class User {
     private LocalDate birthday;
     @Column(name = "country",nullable = false)
     private String country;
+    @Column(length = 30, unique = true)
+    private String username;
+    @Column(length = 200)
+    private String password;
+    private Boolean enabled;
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_user")
+    private List<Role> roles;
+
     public User() {
     }
 
-    public User(int idUser, String name, String email,String phone, LocalDate birthday, String country) {
-
-        this.idUser= idUser;
-        this.name = name;
-        this.email = email;
-        this.phone=phone;
-        this.birthday = birthday;
-        this.country = country;
+    public Long getId() {
+        return id;
     }
 
-    public int getIdUser() {
-        return idUser;
-    }
-
-    public void setIdUser(int idUser) {
-        this.idUser = idUser;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -80,4 +80,35 @@ public class User {
         this.country = country;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 }
