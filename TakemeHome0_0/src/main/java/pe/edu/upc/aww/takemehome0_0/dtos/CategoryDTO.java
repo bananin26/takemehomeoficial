fd24f33e14021cc050bc2e4b07b96d467012338b
Adriano <gian13088@gmail.com>
2023-09-19 01:15:49 -0500

    CategorySecurity

--- TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/dtos/CategoryDTO.java
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/dtos/CategoryDTO.java
@@ -1,26 +1,9 @@
-package pe.edu.upc.aww.takemehome0_0.entities;
+package pe.edu.upc.aww.takemehome0_0.dtos;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name="categories")
-public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class CategoryDTO {
     private int idCategory;
-    @Column(name = "nameCategory",length = 45,nullable = false)
     private String nameCategory;
-    @Column(name = "detailsCategory",length = 45,nullable = false)
     private String detailsCategory;
-
-    public  Category(){
-
-    }
-    public Category(int idCategory,String nameCategory, String detailsCategory){
-        this.idCategory=idCategory;
-        this.nameCategory=nameCategory;
-        this.detailsCategory=detailsCategory;
-    }
 
     public int getIdCategory() {
         return idCategory;
