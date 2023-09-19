7ca10982b486c254a23426f77e7100adc6e47c54
bananin26 <mabeblue26@gmail.com>
2023-09-19 09:21:17 -0500

    security

---
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/controllers/RoleController.java
@@ -0,0 +1,37 @@
+package pe.edu.upc.aww.takemehome0_0.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aww.takemehome0_0.dtos.RoleDTO;
+import pe.edu.upc.aww.takemehome0_0.dtos.UserDTO;
+import pe.edu.upc.aww.takemehome0_0.entities.Role;
+import pe.edu.upc.aww.takemehome0_0.entities.User;
+import pe.edu.upc.aww.takemehome0_0.serviceinterfaces.IRolesService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/Role")
+public class RoleController {
+    @Autowired
+    private IRolesService rS;
+
+    @PostMapping
+    public void register(@RequestBody RoleDTO dto){
+        ModelMapper m = new ModelMapper();
+        Role r= m.map(dto, Role.class);
+        rS.insert(r);
+    }
+
+    @GetMapping
+    public List<RoleDTO> to_list(){
+        return rS.list().stream().map(x->{
+            ModelMapper m= new ModelMapper();
+            return m.map(x,RoleDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+}
