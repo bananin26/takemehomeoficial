227976d0f49cb503cc3d9ad5319014fa995b1209
PieroLoa <piero.loarte32@gmail.com>
2023-09-21 14:50:41 -0500

    Hola

--- TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/controller/OrderController.java
+++ TakemeHome0_0/src/main/java/pe/edu/upc/aww/takemehome0_0/controller/OrderController.java
@@ -1,11 +1,18 @@
 package pe.edu.upc.aww.takemehome0_0.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("Orders")
 public class OrderController {
+    @Autowired
+    private IOrderService oR;
     @GetMapping("/SumatoryPurchasesByUser")
     public List<TotalPurchasesByUserDTO> SumatoriaComprasPorUsuario() {
         List<String[]> lista=oR.totalPurchasesByPerson();
